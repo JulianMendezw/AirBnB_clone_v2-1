ab0c4a9f1a5f08a879f9f1f1e8e3753ab83b5c9b
Julian <julianlx@hotmail.com>
2021-05-07 14:32:49 -0500

    adding debug mode

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ from flask import Flask
 from models import storage
 from flask import Blueprint
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, make_response
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -20,7 +20,7 @@ def teardown_appcontext_func(error):
 @app.errorhandler(404)
 def page_not_found(e):
     """ Custon error 404 handler """
-    return (jsonify({"error": "Not found"}), 404)
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
