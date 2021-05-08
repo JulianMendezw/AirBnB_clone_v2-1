e2a8ca444a3370b69de5fd389a13cf739cf72774
Julian <julianlx@hotmail.com>
2021-05-08 12:48:54 -0500

    adding pretty print to config

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,7 @@ from flask import jsonify, make_response
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
